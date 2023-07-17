e8c699de894c1679e654fe5cb7f54e92ad17b92e
raisariddhi <117309407+raisariddhi@users.noreply.github.com>
2023-07-17 15:02:23 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,39 +1,39 @@
+let secretNumber;
+
 function refreshGame() {
-    fetch('/new-number')
-      .then(() => {
-        const messageElement = document.querySelector('.message');
-        messageElement.innerText = 'Take a guess between 1 and 100:';
-        messageElement.style.color = 'black';
-      })
-      .catch(error => console.error('Error refreshing the game:', error));
+  secretNumber = Math.floor(Math.random() * 100) + 1;
+  const messageElement = document.querySelector('.message');
+  messageElement.innerText = 'Take a guess between 1 and 100:';
+  messageElement.style.color = 'black';
+}
+
+function makeGuess() {
+  const guess = parseInt(document.getElementById('guessInput').value);
+  
+  if (isNaN(guess)) {
+    displayMessage('Invalid guess. Please enter a number.', 'black');
+    return;
   }
-  
-async function makeGuess() {
-    const guess = document.getElementById('guessInput').value;
-    const response = await fetch('/guess', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ guess: guess })
-    });
-  
-    const data = await response.json();
-    const messageElement = document.querySelector('.message');
-    messageElement.innerText = data.message;
-  
-    if (data.message.includes('correct number')) {
-      messageElement.style.color = 'green';
-    } else {
-      messageElement.style.color = 'black';
-    }
+
+  if (guess === secretNumber) {
+    displayMessage(`Congratulations! ${guess} is the correct number!`, 'green');
+  } else if (guess < secretNumber) {
+    displayMessage('Too low! Try a higher number.', 'black');
+  } else if (guess > secretNumber) {
+    displayMessage('Too high! Try a lower number.', 'black');
   }
-  
-  function handleKeyPress(event) {
-    if (event.keyCode === 13) {
-      makeGuess();
-    }
+}
+
+function displayMessage(message, color) {
+  const messageElement = document.querySelector('.message');
+  messageElement.innerText = message;
+  messageElement.style.color = color;
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    makeGuess();
   }
-  
-  document.getElementById('guessInput').addEventListener('keydown', handleKeyPress);
-  
+}
+
+document.getElementById('guessInput').addEventListener('keydown', handleKeyPress);
