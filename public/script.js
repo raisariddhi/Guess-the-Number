d26feedacd2f7dcdaf03d34fd893025d01448051
raisariddhi <117309407+raisariddhi@users.noreply.github.com>
2023-07-17 16:06:35 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,33 +1,30 @@
 function refreshGame() {
-    fetch('/new-number')
-      .then(() => {
-        const messageElement = document.querySelector('.message');
-        messageElement.innerText = 'Take a guess between 1 and 100:';
-        messageElement.style.color = 'black';
-      })
-      .catch(error => console.error('Error refreshing the game:', error));
+  const messageElement = document.querySelector('.message');
+  messageElement.innerText = 'Take a guess between 1 and 100:';
+  messageElement.style.color = 'black';
+}
+
+  
+function makeGuess() {
+  const guess = parseInt(document.getElementById('guessInput').value);
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+  const messageElement = document.querySelector('.message');
+  if (guess === secretNumber) {
+    messageElement.innerText = `Congratulations! ${guess} is the correct number!`;
+    messageElement.style.color = 'green';
+  } else if (guess < secretNumber) {
+    messageElement.innerText = 'Too low! Try a higher number.';
+    messageElement.style.color = 'black';
+  } else if (guess > secretNumber) {
+    messageElement.innerText = 'Too high! Try a lower number.';
+    messageElement.style.color = 'black';
+  } else {
+    messageElement.innerText = 'Invalid guess. Please enter a number.';
+    messageElement.style.color = 'black';
   }
-  
-async function makeGuess() {
-    const guess = document.getElementById('guessInput').value;
-    const response = await fetch('/guess', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ guess: guess })
-    });
-  
-    const data = await response.json();
-    const messageElement = document.querySelector('.message');
-    messageElement.innerText = data.message;
-  
-    if (data.message.includes('correct number')) {
-      messageElement.style.color = 'green';
-    } else {
-      messageElement.style.color = 'black';
-    }
-  }
+}
+
   
   function handleKeyPress(event) {
     if (event.keyCode === 13) {
